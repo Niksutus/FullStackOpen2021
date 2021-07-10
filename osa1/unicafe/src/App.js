f24c3b476356ca068f38175a489bfd80c93b19ca
Niksutus <niko.kananenfi@gmail.com>
2021-07-10 12:20:22 +0300

    muutokset tehty tehtävän 1.8 mukaisesti

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const addGoodVote = () => setGood(good + 1)
-  const addNeutralVote = () => setNeutral(neutral +1)
-  const addBadVote = () => setBad(bad +1)
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
 
   return (
     <div>
-
-      <section>
-        <h1>give feedback</h1>
-        <button onClick={addGoodVote}>good</button>
-        <button onClick={addNeutralVote}>neutral</button>
-        <button onClick={addBadVote}>bad</button>
-      </section>
-
-      <section>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
-        <p>positive {(good/(good+ neutral + bad)) * 100} %</p>
-      </section>
-
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGoodVote = () => setGood(good + 1);
+  const addNeutralVote = () => setNeutral(neutral + 1);
+  const addBadVote = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={addGoodVote}>good</button>
+      <button onClick={addNeutralVote}>neutral</button>
+      <button onClick={addBadVote}>bad</button>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
