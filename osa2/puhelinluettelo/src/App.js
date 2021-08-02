7cf16f95d766694545cd85423248e63f5cd21433
Niksutus <niko.kananenfi@gmail.com>
2021-08-02 12:40:12 +0300

    yritin tehtävää 2.9 mutta en saanut sitä toimimaan

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "0401234567" }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  // const [ filteredNameList, setFilteredNameList ] = useState([])
+
+  
+  const nameArray = persons.map(person => person.name)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -15,8 +21,6 @@ const App = () => {
       number: newNumber,
     }
 
-    const nameArray = persons.map(person => person.name)
-    
     if(nameArray.indexOf(newName) !== -1){
       alert(`${newName} is already added to phonebook`)
     } else {
@@ -35,9 +39,34 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  // Filter 
+  
+  // const handleFilterChange = (event) => {
+    
+  //   for(let i = 0; i < filteredNameList.length; i++){
+  //     setFilteredNameList(filteredNameList.pop(filteredNameList[i]))
+  //     return
+  //   }
+
+  //   let filter = event.target.value.toLowerCase();
+  //   let array = [];
+
+  //   for(let i = 0; i < persons.length; i++){
+  //     if (persons[i].name.toLowerCase().includes(filter)) {
+  //       array.push(persons[i])
+  //     }
+  //   }
+
+  //   setFilteredNameList(filteredNameList.concat(array));
+  // }
+ 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input/>
+        </div>
+      <h2>add a new</h2>
       <form onSubmit = {addPerson}>
         <div>
           name: <input onChange={handleNameChange} value={newName}/>
