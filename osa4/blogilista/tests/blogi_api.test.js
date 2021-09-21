097e2727406e0f155a400c6a6400fcb269a83553
Niksutus <niko.kananenfi@gmail.com>
2021-09-21 12:26:24 +0300

    muutokset tehty tehtävän 4.10 mukaisesti. Myös test_helper.js lisätty

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -1,64 +1,16 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }  
-]
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  const blogObjects = blogs
+  const blogObjects = helper.initalBlogs
     .map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
@@ -70,16 +22,38 @@ test('right amount of blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(blogs.length)
+  expect(response.body).toHaveLength(helper.initalBlogs.length)
 })
 
-test('blogien identifioivan kentän nimi on id', async () => {
+test('the field identifying the blogs is named id', async () => {
   const response = await api
     .get('/api/blogs/')
   
   for (let blog of response.body){
     expect(blog.id).toBeDefined()
   }
+})
+
+test('a blog with the right contents gets added to the database', async () => {
+
+  const newBlog = {
+    title: "Test Title",
+    author: "Test Author",
+    url: "Test Url",
+    likes: 20
+  }
+
+  await api
+    .post('/api/blogs/')
+    .send(newBlog)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const titles = blogsAtEnd.map(blog => blog.title)
+
+  expect(blogsAtEnd).toHaveLength(helper.initalBlogs.length +1 )
+  expect(titles).toContain(
+    'Test Title'
+  )
 })
 
 afterAll(() => {
