16802e6ea813a82b3726ab9952398c44c2f1d59d
Niksutus <niko.kananenfi@gmail.com>
2021-09-28 16:33:15 +0300

    muutokset tehty tehtävän4.18 mukaisesti

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -14,5 +15,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
