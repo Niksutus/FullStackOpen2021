724001cebf7a7a6d21acb0519a0554ad6c03e335
Niksutus <niko.kananenfi@gmail.com>
2021-10-01 13:15:56 +0300

    muutokset tehty tehtävän 5.2 mukaisesti

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -15,12 +15,25 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+
+    if(loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
  
   const loginHandler = async (event) => {
     event.preventDefault()
     
     try{
       const user = await loginService.login({ username, password, })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -29,7 +42,12 @@ const App = () => {
       console.log(exception)
     }
     
-  } 
+  }
+  
+  const logoutHandler = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
 
   if(user === null) {
     return(
@@ -63,7 +81,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} is logged in</p>
+      <p>{user.name} is logged in <button onClick={logoutHandler}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
